36304d2363e522e3cf242e07f4ce04c784663814
Bence Nagy <bence@underyx.me>
2021-06-30 17:43:21 +0300

    Add website tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import * as FullStory from "@fullstory/browser";
+
+FullStory.init({ orgId: "112R81" });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
