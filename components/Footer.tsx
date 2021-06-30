2b13de5d556525a100b8eb75de05f76327c561d7
Bence Nagy <bence@underyx.me>
2021-06-30 18:43:45 +0300

    Styling updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,16 @@
 export default function Footer() {
   return (
-    <footer className="footer has-background-white-bis has-text-dark has-text-centered">
-      Hey, I&rsquo;m <a href="https://underyx.me">Bence Nagy</a> and I made this
-      website while waiting for my CR-1 visa.
-      <br />
-      If you found it useful, consider{" "}
-      <a href="https://ko-fi.com/underyx">getting me a coffee</a> &mdash; who
-      knows, maybe your donation will end up on my form I&#8209;864. Good luck
-      to you all!
-    </footer>
+    <div className="container is-size-6 is-max-desktop my-5">
+      <footer className="footer has-background-white-bis has-text-dark has-text-centered">
+        Hey, I&rsquo;m <a href="https://underyx.me">Bence Nagy</a> and I made
+        this website while waiting for my CR-1 visa.
+        <br />
+        If you found it useful, consider{" "}
+        <a href="https://ko-fi.com/underyx">getting me a coffee</a>. Who knows,
+        maybe your donation will end up on my I&#8209;864.
+        <br />
+        <strong>Good luck to you all!</strong>
+      </footer>
+    </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,53 +9,55 @@ export default function Navbar() {
       role="navigation"
       aria-label="main navigation"
     >
-      <div className="navbar-brand">
-        <Link href="/">
-          <a className="navbar-item">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/logo.svg" alt="Logo for VisaWhen" />
-            &nbsp;
-            <b>VisaWhen</b>
+      <div className="container is-size-6 is-max-desktop">
+        <div className="navbar-brand">
+          <Link href="/">
+            <a className="navbar-item">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/logo.svg" alt="Logo for VisaWhen" />
+              &nbsp;
+              <b>VisaWhen</b>
+            </a>
+          </Link>
+
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </a>
-        </Link>
-
-        <a
-          role="button"
-          className="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <Link href="/nvc">
-            <a className="navbar-item">NVC</a>
-          </Link>
-
-          <Link href="/consulates">
-            <a className="navbar-item">Consulates</a>
-          </Link>
         </div>
 
-        <div className="navbar-end">
-          <div className="navbar-item" style={{ height: "3.25rem" }}>
-            <div className="field is-grouped">
-              <div className="control">
-                <a
-                  href="https://ko-fi.com/underyx"
-                  className="button is-success"
-                >
-                  <span className="icon">
-                    <FontAwesomeIcon icon={faHeart} />
-                  </span>
-                  <span>Buy Bence a coffee</span>
-                </a>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <Link href="/nvc">
+              <a className="navbar-item">NVC</a>
+            </Link>
+
+            <Link href="/consulates">
+              <a className="navbar-item">Consulates</a>
+            </Link>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item" style={{ height: "3.25rem" }}>
+              <div className="field is-grouped">
+                <div className="control">
+                  <a
+                    href="https://ko-fi.com/underyx"
+                    className="button is-success has-text-weight-semibold"
+                  >
+                    <span className="icon">
+                      <FontAwesomeIcon icon={faHeart} />
+                    </span>
+                    <span>Buy Bence a coffee</span>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
