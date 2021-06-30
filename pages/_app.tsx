4a44531266ee582bf83f96d1dd9150269916eec1
Bence Nagy <bence@underyx.me>
2021-06-30 21:45:38 +0300

    Fix tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,5 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-3QQ9KQ0WCE");
-ReactGA.send("pageview");
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,15 +8,12 @@ import Document, {
 import React from "react";
 import Navbar from "../components/Navbar";
 import PageWrapper from "../components/PageWrapper";
-import FullStory from "react-fullstory";
 
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
 // Prevent fontawesome from adding its CSS since we did it manually.
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; /* eslint-disable import/first */
-
-const FULLSTORY_ORG_ID = "112R81";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -51,6 +48,49 @@ class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-3QQ9KQ0WCE"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-3QQ9KQ0WCE');
+              `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window['_fs_debug'] = false;
+                window['_fs_host'] = 'fullstory.com';
+                window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
+                window['_fs_org'] = '112R81';
+                window['_fs_namespace'] = 'FS';
+                (function(m,n,e,t,l,o,g,y){
+                    if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
+                    g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
+                    o=n.createElement(t);o.async=1;o.crossOrigin='anonymous';o.src='https://'+_fs_script;
+                    y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+                    g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
+                    g.anonymize=function(){g.identify(!!0)};
+                    g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};
+                    g.log = function(a,b){g("log",[a,b])};
+                    g.consent=function(a){g("consent",!arguments.length||a)};
+                    g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+                    g.clearUserCookie=function(){};
+                    g.setVars=function(n, p){g('setVars',[n,p]);};
+                    g._w={};y='XMLHttpRequest';g._w[y]=m[y];y='fetch';g._w[y]=m[y];
+                    if(m[y])m[y]=function(){return g._w[y].apply(this,arguments)};
+                    g._v="1.3.0";
+                })(window,document,window['_fs_namespace'],'script','user');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Navbar />
@@ -58,7 +98,6 @@ class MyDocument extends Document {
             <Main />
           </PageWrapper>
           <NextScript />
-          <FullStory org={FULLSTORY_ORG_ID} />
         </body>
       </Html>
     );
