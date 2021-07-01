04099487101c31f4361c798fd2480d37f54a5f96
Bence Nagy <bence@underyx.me>
2021-07-01 12:23:45 +0300

    Much data cleanup

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -4,16 +4,16 @@ import Footer from "./Footer";
 export default function PageWrapper({ children }: React.PropsWithChildren<{}>) {
   return (
     <section className="section has-background-white-bis has-text-dark">
-      <main className="container is-size-6 is-max-desktop">
-        <article className="message is-info">
+      <section className="container is-size-6 is-max-desktop">
+        <aside className="message is-info">
           <div className="message-body">
             Hey there! You&rsquo;re one of the first people to find this brand
             new site. Please share it in Facebook groups, forums, and wherever
             you think it can help others out!
           </div>
-        </article>
-        {children}
-      </main>
+        </aside>
+        <main>{children}</main>
+      </section>
       <Footer />
     </section>
   );
