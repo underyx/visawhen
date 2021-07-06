108a292b03a5768b243392c88a8754c9962b915b
Bence Nagy <bence@underyx.me>
2021-07-06 10:21:31 +0300

    Remove new site banner, fix typos

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -4,16 +4,9 @@ import Footer from "./Footer";
 export default function PageWrapper({ children }: React.PropsWithChildren<{}>) {
   return (
     <section className="section has-background-white-bis has-text-dark">
-      <section className="container is-size-6 is-max-desktop">
-        <aside className="message is-info">
-          <div className="message-body">
-            Hey there! You&rsquo;re one of the first people to find this brand
-            new site. Please share it in Facebook groups, forums, and wherever
-            you think it can help others out!
-          </div>
-        </aside>
-        <main>{children}</main>
-      </section>
+      <article className="container is-size-6 is-max-desktop">
+        {children}
+      </article>
       <Footer />
     </section>
   );
