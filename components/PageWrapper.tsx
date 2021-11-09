413a8a826a1cd6ae8f281bebcb9f5eb8eb6f2237
Bence Nagy <bence@underyx.me>
2021-11-08 19:38:12 -0800

    Add member count to Discord banner

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -7,11 +7,11 @@ export default function PageWrapper({ children }: React.PropsWithChildren<{}>) {
       <section className="container is-size-6 is-max-desktop">
         <aside className="message is-info">
           <div className="message-body">
-            <strong>News from Oct 28th:</strong> Hey, why does the immigration
+            <strong>News from Nov 8th:</strong> Hey, why does the immigration
             community not have a place to chat? I wondered this and next thing I
             know, I created a Discord community for ourselves!{" "}
             <strong>
-              <a href="https://discord.gg/zkf8w2QtQY">Come say hi to us!</a>
+              <a href="https://discord.gg/zkf8w2QtQY">Come say hi to 44 members and counting!</a>
             </strong>
           </div>
         </aside>
