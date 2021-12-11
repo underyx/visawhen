2332a13f050367b3d6dd845a63489ee03be205fe
Bence Nagy <bence@underyx.me>
2021-12-11 11:05:08 -0800

    Remove discord banner

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -5,18 +5,6 @@ export default function PageWrapper({ children }: React.PropsWithChildren<{}>) {
   return (
     <section className="section has-background-white-bis has-text-dark">
       <section className="container is-size-6 is-max-desktop">
-        <aside className="message is-info">
-          <div className="message-body">
-            <strong>News from Nov 24th:</strong> Hey, why does the immigration
-            community not have a place to chat? I wondered this and next thing I
-            know, I created a Discord community for ourselves!{" "}
-            <strong>
-              <a href="https://discord.gg/zkf8w2QtQY">
-                Come say hi to 92 members and counting!
-              </a>
-            </strong>
-          </div>
-        </aside>
         <article>{children}</article>
       </section>
       <Footer />
